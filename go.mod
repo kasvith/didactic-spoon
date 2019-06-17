c8c38458bcba497ea34c94dce02ab59c19efe1fb
Kasun Vithanage <alankasun@gmail.com>
2019-06-17 14:25:15 +0530

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kasvith/awesomeProject1
+
+go 1.12
+
+require (
+	github.com/danieljoos/wincred v1.0.2 // indirect
+	github.com/docker/docker-credential-helpers v0.6.2
+	github.com/stretchr/testify v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/kasvith/awesomeProject1/nativestore"
+	"log"
+)
+
+func main() {
+	err := nativestore.Set("example-key", "https://com.example.g", "admin", "damin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	usr, secret, err := nativestore.Get("example-key", "https://com.example.g")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(usr, secret)
+}
